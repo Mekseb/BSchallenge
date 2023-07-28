93acc96d8ca2697d6c37562bb038c06981a5a9d6
Mekseb <meksebeehari99@gmail.com>
2023-07-28 19:20:15 +0200

    Finishing

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+
+//Postschema
+const postschema = new mongoose.Schema({
+    title:String,
+    content:String,
+});
+
+const Posts = mongoose.model('POSTS',postschema );
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const test = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper"
@@ -15,17 +25,19 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-let posts = [];
+postsarr = [];
 
 app.get("/", function(req, res){
-    res.render("home.ejs",{ 
-        homeStartingContent: homeStartingContent ,
-        posts: posts
-    }
-    
-    );
 
-    //console.log(posts)   
+    postsarr.push(Posts.find())
+ 
+    Posts.find().then(function (post) {
+        
+        res.render("home.ejs",{ 
+            homeStartingContent: homeStartingContent ,
+            posts: post
+        });
+    });
 });
 
 app.get("/contact", function(req, res){
@@ -42,11 +54,13 @@ app.get("/compose", function(req, res){
 
 app.post("/compose", function(req, res){
     
-    const post = {
+    const post = new Posts({
         title:req.body.postTitle,
         content:req.body.postBody
-    }
-    posts.push(post)
+    });
+
+
+    post.save();
     
     //console.log(req);
     res.redirect("/")
@@ -56,18 +70,23 @@ app.post("/compose", function(req, res){
 app.get("/posts/:postName", function(req, res){
     const requestedTitle = _.lowerCase(req.params.postName);
   
-    posts.forEach(function(post){
-      const storedTitle = _.lowerCase(post.title);
-  
-      if (storedTitle === requestedTitle) {
-        res.render("post", {
-          title: post.title,
-          content: post.content
-        });
-      }
-    });
-  
-  });
+  Posts.find().then(function (post) {
+    
+    post.forEach(function(post){
+        const storedTitle = _.lowerCase(post.title);
+    
+        if (storedTitle === requestedTitle) {
+          res.render("post", {
+            title: post.title,
+            content: post.content
+          });
+        }
+      });
+
+});
+
+});
+
 
 app.listen(3100, function() {
     console.log("Server started on port 3100");
